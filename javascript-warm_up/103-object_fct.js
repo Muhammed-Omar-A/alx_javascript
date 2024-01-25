34728ef22c71444e059e5251ce15940be352208f
muhammed omar <2muhammedomar@gmail.com>
2024-01-25 22:34:42 +0200

    tjs

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  // Output: { type: 'object', value: 12 }
+  
+  myObject.incr = function () {
+    this.value++;
+  };
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 13 }
+  
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 14 }
+  
+  myObject.incr();
+  console.log(myObject);
+  // Output: { type: 'object', value: 15 }
+  
+  
+  
+  
+  
+  
+  
+    
